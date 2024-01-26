b2ae23ae08e1e401f8208491c0f938ec0ea57f79
yevhen <shcherbatyke@gmail.com>
2024-01-26 17:29:08 +0200

    first commit

---
+++ internal/api/note/create.go
@@ -0,0 +1,19 @@
+package note
+
+import (
+	"context"
+	"log"
+)
+
+func (i *Implementation) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	id, err := i.noteService.Create(ctx, converter.ToNoteInfoFromDesc(req.GetInfo()))
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("inserted note with id: %d", id)
+
+	return &desc.CreateResponse{
+		Id: id,
+	}, nil
+}
